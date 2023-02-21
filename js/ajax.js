6e5d875b4812bcaea85de675bc1b854bd0d2b0a1
Akash Kumar <108467744+0x1ak4sh@users.noreply.github.com>
2023-02-21 20:35:36 +0000

    Create ajax.js

---
+++ js/ajax.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+      $('form').submit(function(event) {
+        event.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+          type: 'POST',
+          url: 'https://mrakashkumar.000webhostapp.com/send_email.php',
+          data: formData,
+          success: function(response) {
+            console.log(response);
+          }
+        });
+      });
+    });
