856268e9e678f8938b37f641fc1a643fb041c5a4
Akash Kumar <108467744+0x1ak4sh@users.noreply.github.com>
2023-02-21 21:14:56 +0000

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
- $(document).ready(function() {
+ jQuery(document).ready(function() {
       $('form').submit(function(event) {
         event.preventDefault();
         var formData = $(this).serialize();
